5ec05a763c7a518add5536cb10531e71e2c53497
Aash <aash.kothari@hiteshi.com>
2018-08-05 23:44:38 +0530

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,19 @@
+export default function(){
+    return [
+        {
+            'key':1,
+            'title':'Aash',
+            'author':'AA'
+        },
+        {
+            'key':2,
+            'title':'Manoj',
+            'author':'BA'
+        },
+        {
+            'key':3,
+            'title':'Rajendra',
+            'author':'CA'
+        }
+    ]
+}
